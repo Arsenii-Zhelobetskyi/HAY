b4aed698687f075cc15ec4b7a9c760c6d9cd2fd7
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2024-05-01 16:40:23 +0300

    pagination almost completed

--- src/components/Products/ThePagination.vue
+++ src/components/Products/ThePagination.vue
@@ -1,41 +1,71 @@
 <script setup>
 import { ref, watch } from 'vue'
+const DOTS = '...'
+const currentPage = ref(1)
+const contentCount = ref(80)
 
-const position = ref(0)
-const length = ref(76)
-const quantityOfPages = ref(7)
-const pagination = ref(
-  Array.from(
-    { length: length.value > quantityOfPages.value ? quantityOfPages.value : length.value },
-    (_, index) => index + 1
-  )
-)
-
-function changePosition(value) {
-  position.value = value
+function range(start, end) {
+  const length = end - start + 1
+  return Array.from({ length }, (_, index) => index + start)
 }
-watch(position, (value) => {
-  if (value < pagination.value.length - 1) {
-    return
-  } else {
-    pagination.value = pagination.value.slice(value - 3)
-    pagination.value.push(value + 2)
+
+function usePagination(totalCount, pageSize, siblingCount = 2, currentPage) {
+  const totalPageCount = Math.ceil(totalCount / pageSize)
+
+  const totalPageNumbers = siblingCount + 5
+
+  console.log(totalPageNumbers)
+  if (totalPageNumbers >= totalPageCount) {
+    return range(1, totalPageCount)
   }
+
+  const leftSiblingIndex = Math.max(currentPage - siblingCount, 1)
+  const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount)
+
+  const shouldShowLeftDots = leftSiblingIndex > 2
+  const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2
+
+  const firstPageIndex = 1
+  const lastPageIndex = totalPageCount
+
+  if (!shouldShowLeftDots && shouldShowRightDots) {
+    const leftItemCount = 3 + 2 * siblingCount
+    const leftRange = range(1, leftItemCount)
+
+    return [...leftRange, DOTS, totalPageCount]
+  }
+  if (shouldShowLeftDots && !shouldShowRightDots) {
+    const rightItemCount = 3 + 2 * siblingCount
+    const rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount)
+    return [firstPageIndex, DOTS, ...rightRange]
+  }
+  if (shouldShowLeftDots && shouldShowRightDots) {
+    const middleRange = range(leftSiblingIndex, rightSiblingIndex)
+    return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex]
+  }
+}
+
+let pagination = usePagination(contentCount.value, 10, 2, currentPage.value)
+
+watch(currentPage, (value) => {
+  pagination = usePagination(contentCount.value, 10, 2, value)
 })
+
+function changePosition(value, index) {
+  currentPage.value = value
+}
 </script>
 <template>
-  <div class="justify-cente flex items-center">
-    <div :v-if="length > 7 && position < 6" class="flex gap-1">
+  <div class="flex items-center justify-center">
+    <div class="flex gap-1">
       <div
         v-for="(page, index) in pagination"
         :key="index"
-        @click="changePosition(page)"
-        :class="`${position === page ? 'bg-gray-300' : 'bg-gray-100'} cursor-pointer rounded-md p-2`"
+        @click="changePosition(page, index)"
+        :class="`${currentPage == page ? 'bg-blue-700' : 'bg-red-100'} cursor-pointer rounded-md p-2`"
       >
-        {{ page }}
+        {{ page === DOTS ? DOTS : page }}
       </div>
-      <div>...</div>
-      <div>{{ length }}</div>
     </div>
   </div>
 </template>
