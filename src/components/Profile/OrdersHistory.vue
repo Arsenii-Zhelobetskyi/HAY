7183757659c6b2aec9ec7c378ccf4e2d271c363f
MisakiZNU <fee@st.znu.edu.ua>
2024-06-05 00:52:26 +0300

    fix multi products

--- src/components/Profile/OrdersHistory.vue
+++ src/components/Profile/OrdersHistory.vue
@@ -31,11 +31,11 @@ onMounted(async () => {
       <h2 class="text-2xl font-bold mb-4" v-if="orders.length > 0">Your Orders:</h2>
       <h2 class="text-2xl font-bold mb-4" v-else>No orders found.</h2>
       <ul class="space-y-3">
-        <li v-for="order in orders" :key="order.id" class="flex items-center p-4 bg-white drop-shadow-md">
-          <img :src="order.images" alt="Product Image" class="w-36 h-36 object-cover mr-4">
+        <li v-for="order in orders" :key="order.id" class="flex items-center p-4 bg-white drop-shadow-lg rounded-md">
+          <img :src="order.images" alt="Product Image" class="w-36 h-36 object-cover mr-4 rounded-md">
           <div>
             <div class="text-lg font-semibold -mt-1">{{ order.name }}</div>
-            <div class="text-gray-500 mt-16">Total price: {{ order.total_price }}</div>
+            <div class="text-gray-600 mt-16">Total price: {{ order.total_price }}$</div>
           </div>
           <div class="ml-auto text-lg text-gray-700 mt-36">Quantity: {{ order.products_bought }}</div>
         </li>
