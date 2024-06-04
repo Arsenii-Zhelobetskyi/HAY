0bcd69381732ff966678589f04550ca52c3f5a13
Kateryna Pazynych <pazynych_ka@st.znu.edu.ua>
2024-06-04 23:45:06 +0300

    NotFound page

---
+++ src/ui/TheArrow.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    v-motion
+    :initial="{
+      scale: 1,
+      y: -10,
+      z: 0
+    }"
+    :enter="{
+      scale: 1.2,
+      y: 10,
+      z: 10,
+      transition: {
+        duration: 1000,
+        repeat: Infinity,
+        repeatType: 'mirror'
+      }
+    }"
+    class="arrow"
+  >
+    <svg
+      class="arrow-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="48"
+      height="48"
+      fill="currentColor"
+    >
+      <path
+        d="M12 2a1 1 0 0 1 1 1v15.59l4.3-4.3a1 1 0 1 1 1.4 1.42l-6 6a1 1 0 0 1-1.4 0l-6-6a1 1 0 1 1 1.4-1.42l4.3 4.3V3a1 1 0 0 1 1-1z"
+      />
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.arrow {
+  font-size: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,53 @@
-<script setup></script>
+<script setup>
+import sidebarSignIn from '@/assets/sidebar-signIn.jpg'
+import TheArrow from '../ui/TheArrow.vue'
+</script>
+
 <template>
-  <div>
-    <h1>Not Found</h1>
-  </div>
+  <main class="grid grid-cols-3 font-normal">
+    <section class="col-span-1 flex items-center justify-center px-10 pb-20 pt-8">
+      <div
+        class="flex max-w-96 flex-col items-center justify-center gap-10 text-2xl font-light text-gray-600"
+      >
+        <div class="text-4xl font-medium text-black">
+          Oops, the page with this URL does not exist...
+        </div>
+        <div>Let's go choose a new table and chair for you</div>
+        <the-arrow></the-arrow>
+        <RouterLink to="/products" class="btn border-4 border-gray-600 px-10 py-4 font-medium">
+          Our Products
+        </RouterLink>
+      </div>
+    </section>
+    <aside class="relative col-span-2 h-screen w-full">
+      <img
+        v-motion
+        :initial="{
+          y: -850
+        }"
+        :enter="{
+          y: 0,
+          transition: {
+            duration: 2000
+          }
+        }"
+        :src="sidebarSignIn"
+        alt="Sign in"
+        class="block h-full w-full object-cover object-bottom"
+      />
+    </aside>
+  </main>
 </template>
+
+<style scoped>
+.btn {
+  background-color: none;
+  color: rgb(75 85 99);
+  transition: all 0.6s ease;
+}
+
+.btn:hover {
+  background-color: rgb(75 85 99);
+  color: rgb(255 255 255);
+}
+</style>
