bde34236b26fa68bbfda3592e2179fb6b3a6b413
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2024-07-01 16:34:43 +0300

    fix for pagination and netlify

--- src/components/Products/ThePagination/ThePagination.vue
+++ src/components/Products/ThePagination/ThePagination.vue
@@ -32,20 +32,21 @@ const { pagination, goBack, goForward, currentPage, Dots, changePosition } = use
     class="mb-20 mt-8 flex items-center justify-center text-2xl"
   >
     <div class="flex items-center justify-center gap-14">
-      <div class="cursor-pointer px-4 py-2 font-medium hover:bg-gray-200" @click="goBack">
+      <div v-hoverable class="px-4 py-2 font-medium hover:bg-gray-200" @click="goBack">
         &#x3c; Previous
       </div>
       <div class="flex gap-1">
         <div
           v-for="(page, index) in pagination"
+          v-hoverable
           :key="index"
           @click="changePosition(page)"
-          :class="`${currentPage == page ? ' bg-black px-8  font-medium text-white' : ' hover:bg-gray-200'} cursor-pointer   px-4 py-2`"
+          :class="`${currentPage == page ? ' bg-black px-8  font-medium text-white' : ' hover:bg-gray-200'}   px-4 py-2`"
         >
           {{ page instanceof Dots ? '&#8230;' : page }}
         </div>
       </div>
-      <div class="cursor-pointer px-4 py-2 font-medium hover:bg-gray-200" @click="goForward">
+      <div v-hoverable class="px-4 py-2 font-medium hover:bg-gray-200" @click="goForward">
         Next &#x3e;
       </div>
     </div>
